e4b262c1465324c66728d6a435b08c157d2f939e
Orxan <orxan@skybox.studio>
2019-11-07 21:30:53 +0400

    Add annotation for models

--- ServiceLayer/Models/User.cs
+++ ServiceLayer/Models/User.cs
@@ -1,4 +1,6 @@
-﻿namespace ServiceLayer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceLayer.Models
 {
     /// <summary>
     /// Класс представляющий пользователей
@@ -8,22 +10,30 @@
         /// <summary>
         /// Уникальный айди юзера
         /// </summary>
+        [Key]
         public string ID { get; set; }
         /// <summary>
         /// Имя юзера
         /// </summary>
+        [Required]
+        [MaxLength(50)]
         public string Nickname { get; set; }
         /// <summary>
         /// Почтовый адресс
         /// </summary>
+        [Required]
+        [MaxLength(30)]
         public string Email { get; set; }
         /// <summary>
         /// Пароль
         /// </summary>
+        [Required]
+        [MinLength(8)]
         public string Password { get; set; }
         /// <summary>
         /// Статус юзера
         /// </summary>
+        [Required]
         public string Status { get; set; }
         //TODO дополнить модель по мере надобности
     }
