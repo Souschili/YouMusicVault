224b3579ffb8e08b98db0bcf518a5563db2a9bd6
Orxan <orxan@skybox.studio>
2019-11-09 05:10:43 +0400

    Добавленна модель для регистрации юзера

---
+++ ServiceLayer/ViewModels/UserRegistrationModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.ViewModels
+{
+    /// <summary>
+    /// Класс для передачи данных юзера для регистрации
+    /// </summary>
+    public class UserRegistrationModel
+    {
+        /// <summary>
+        /// Общедоступное имя пользователя
+        /// </summary>
+        public string NickName { get; set; }
+        /// <summary>
+        /// Почтовый адресс
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; }
+
+        //TODO добавить поля по мере надобности  
+    }
+}
