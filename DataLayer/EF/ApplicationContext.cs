1ff9936638c7c0eb745939c485402966b93d6662
Orxan <orxan@skybox.studio>
2019-11-09 06:14:54 +0400

    Автоформатирование и добавление коментариев

--- DataLayer/EF/ApplicationContext.cs
+++ DataLayer/EF/ApplicationContext.cs
@@ -1,18 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ServiceLayer.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DataLayer.EF
 {
+    /// <summary>
+    /// Класс содержащий таблицы и отвечающий за работу с БД
+    /// </summary>
     public class ApplicationContext : DbContext
     {
-        public DbSet<User> User {get;set;}
-    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
-    {
-        Database.EnsureCreated();
-    }
+        /// <summary>
+        /// Таблица пользователей приложения
+        /// </summary>
+        public DbSet<User> User { get; set; }
 
-}
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="options">дефолтные настройки для передачи в базовый класс</param>
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
 }
