348643e7570f82cf8983cf391643e6abd95d17f6
Orxan <orxan@skybox.studio>
2019-11-11 21:22:08 +0400

    Добавлена вью модель для регистрации

---
+++ ServiceLayer/ViewModels/UserLogInModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.ViewModels
+{
+    public class UserLogInModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
