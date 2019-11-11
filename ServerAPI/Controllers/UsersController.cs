755e6477961bbf27749a08de18fa125a00e1778b
Orxan <orxan@skybox.studio>
2019-11-11 20:32:56 +0400

    Добавлен контролер по работе с пользователями UsersController

---
+++ ServerAPI/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Services;
+
+namespace ServerAPI.Controllers
+{
+    /// <summary>
+    /// Работа с текущими пользователями
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserManager userManager;
+
+        public UsersController(IUserManager manager)
+        {
+            userManager = manager;
+        }
+
+
+
+    }
+}
