8a4c7f3509bce0ace74eebfe7250eda003d7124e
Orxan <orxan@skybox.studio>
2019-11-11 21:06:52 +0400

    Добавлен интерфейс и класс по генерации токенов

---
+++ ServerAPI/Options/GlobalTokenGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerAPI.Options
+{
+    /// <summary>
+    /// Класс отвечающий за генерацию токенов
+    /// </summary>
+    public class GlobalTokenGenerator : ITokenGenerator
+    {
+        
+        public Task GenerateJwtToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
