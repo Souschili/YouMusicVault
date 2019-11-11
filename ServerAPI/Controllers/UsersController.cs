d62b98b0f1a2099c1fad1f29fd0a2ebdf0d46cb9
Orxan <orxan@skybox.studio>
2019-11-11 20:39:01 +0400

    Перенесены два метода All и GetAllAsync  из Auth в Users controller

--- ServerAPI/Controllers/UsersController.cs
+++ ServerAPI/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.Models;
 using ServiceLayer.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ServerAPI.Controllers
 {
@@ -17,6 +20,28 @@ namespace ServerAPI.Controllers
             userManager = manager;
         }
 
+        /// <summary>
+        /// Показ всех имеющихся пользователей
+        /// </summary>
+        /// <returns>Список пользователей</returns>
+        [HttpGet("all")]
+        [Produces("application/json")]
+        public async Task<ActionResult<List<User>>> All()
+        {
+            return await userManager.FindAllUsers();
+        }
+
+        /// <summary>
+        /// Получить юзера по нику
+        /// </summary>
+        /// <param name="name">Никнейм пользователя</param>
+        /// <returns></returns>
+        [HttpGet("{name}")]
+        public async Task<ActionResult<User>> GetUserAsync(string name)
+        {
+            var user = await userManager.FindUserByNickname(name);
+            return user;
+        }
 
 
     }
