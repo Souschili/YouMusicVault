145490a6ada95dbe939ce2116cc0035652e65708
Orxan <orxan@skybox.studio>
2019-11-12 18:46:16 +0400

    Добавлена модель передачи токенов из серверной части в клиентскую

---
+++ ServerAPI/Options/JWTModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerAPI.Options
+{
+    /// <summary>
+    /// Класс возрающий токен и прочую инфу
+    /// </summary>
+    public class JWTModel
+    {
+
+        public string JwtToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
